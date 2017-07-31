c50d8bde990277840b343befc49785381b7f4496
Sebastian Appelt <sappelt@code-plus.de>
2017-07-31 14:34:56 +0200

    Added cube sample

---
+++ EvadeVR/Assets/Scripts/AddCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCube : MonoBehaviour {
+
+    public GameObject myCube;
+
+	// Use this for initialization
+	void Start () {
+        GameObject cube = Instantiate(myCube);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
