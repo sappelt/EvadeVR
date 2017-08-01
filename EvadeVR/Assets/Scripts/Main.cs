ce4ccded94fb5166d14f0b2ee6ceb3e67d95e7e7
vHentschel <valerie.hentschel@yahoo.de>
2017-08-01 13:56:08 +0200

    save all trace files from resource folder into list

--- EvadeVR/Assets/Scripts/Main.cs
+++ EvadeVR/Assets/Scripts/Main.cs
@@ -3,43 +3,48 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
     TimeStep timestep;
-    List<TimeStep> timeSteps = new List<TimeStep>();
+    List<Dictionary<int, Vector3>> dictList = new List<Dictionary<int, Vector3>>();
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public List<TimeStep> parseCSV(string path)
+    // Use this for initialization
+    void Start()
     {
-        try
-        {
-            using (StreamReader readFile = new StreamReader(path))
-            {
-                string line;
-                string[] row;
-
-                while ((line = readFile.ReadLine()) != null)
-                {
-                    row = line.Split(' ');
-                    TimeStep timestep = new TimeStep(int.Parse(row[0]), int.Parse(row[1]), int.Parse(row[2]));
-                    timeSteps.Add(timestep);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            //@TODO Exception
-        }
-
-        return timeSteps;
+        parseCSV(Application.dataPath+"\\Resources");
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void parseCSV(string directoryPath)
+    {
+        string[] files = Directory.GetFiles(directoryPath);
+        Dictionary<int, Vector3> itemDict;
+        foreach (string file in files)
+        {
+            if (!file.EndsWith(".csv"))
+                continue;
+
+            itemDict = new Dictionary<int, Vector3>();
+
+            Debug.Log(file);
+            string sFileContents = new StreamReader(File.OpenRead(file)).ReadToEnd();
+
+            string[] sFileLines = sFileContents.Split('\n');
+
+            for (int i = 1; i < sFileLines.Length - 1; i++)
+            {
+                String[] line = sFileLines[i].Split(' ');
+                int time = int.Parse(line[2]);
+
+                itemDict.Add(time, new Vector3() { x = float.Parse(line[0]), y = 0, z = float.Parse(line[1]) });
+            }
+            dictList.Add(itemDict);
+        }
+        
+    }
 }
