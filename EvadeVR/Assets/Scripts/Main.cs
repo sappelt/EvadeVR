2d663a595da145c5b2c99ad869d8cc8c36969aea
vHentschel <valerie.hentschel@yahoo.de>
2017-08-01 10:12:23 +0200

    Blubb

---
+++ EvadeVR/Assets/Scripts/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+    TimeStep timestep;
+    List<TimeStep> timeSteps = new List<TimeStep>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public List<TimeStep> parseCSV(string path)
+    {
+        try
+        {
+            using (StreamReader readFile = new StreamReader(path))
+            {
+                string line;
+                string[] row;
+
+                while ((line = readFile.ReadLine()) != null)
+                {
+                    row = line.Split(' ');
+                    TimeStep timestep = new TimeStep(int.Parse(row[0]), int.Parse(row[1]), int.Parse(row[2]));
+                    timeSteps.Add(timestep);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            //@TODO Exception
+        }
+
+        return timeSteps;
+    }
+
+}
---
+++ EvadeVR/Assets/Scripts/TimeStep.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStep : MonoBehaviour {
+    private int x;
+    private int y;
+    private int time;
+
+    public TimeStep(int x, int y, int time)
+        {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
