25378936cb19d77a5c2d391dc4d963baa84c4337
Peter Gleixner <gleixnerp@gmail.com>
2017-08-03 11:34:30 +0200

    cube and machines

--- EvadeVR/Assets/Scripts/AddCube.cs
+++ EvadeVR/Assets/Scripts/AddCube.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
@@ -56,14 +57,66 @@ public class AddCube : MonoBehaviour {
 		startY = cubeWidth / 2;
 
 		//Get the Prototype Cube and Calculate Values
-		scaleVector = new Vector3 (cubeWidth, cubeWidth, cubeWidth);
+		scaleVector = new Vector3 (cubeWidth, cubeWidth/10, cubeWidth);
 		startVector = new Vector3 (startX, 0, startZ);
 
+		//Draw Machines/Sources/Sinks
+		//TODO: Read from file (not hardcoded shit) and shorten obviously
+		int[][] machines = 
+		{
+			new int[] {7,6},
+			new int[] {4,11},
+			new int[] {6,0},
+			new int[] {5,3},
+			new int[] {10,10},
+			new int[] {3,3},
+		};
+		int[][] sources = 
+		{
+			new int[] {0,1},
+			new int[] {0,4},
+			new int[] {0,7},
+			new int[] {0,10}
+		};
+		int[][] sinks = 
+		{
+			new int[] {11,1},
+			new int[] {11,4},
+			new int[] {11,7},
+			new int[] {11,10}
+		};
+		foreach (int[] machine in machines) {
+			GameObject cubeInstance;
+			cubeInstance = Instantiate(theCube);
+
+			cubeInstance.GetComponent<Renderer> ().material.color = Color.red;
+			cubeInstance.transform.position = Vector3.Scale((new Vector3(machine[0], 1, machine[1])), scaleVector) + startVector;
+			cubeInstance.name = "Machine: " + "(" + machine[0] + "," + machine[1] + ")";
+		}
+		foreach (int[] source in sources) {
+			GameObject cubeInstance; 
+			cubeInstance = Instantiate(theCube);
+			cubeInstance.GetComponent<Renderer> ().material.color = Color.white;
+			cubeInstance.transform.position = Vector3.Scale((new Vector3(source[0], 1, source[1])), scaleVector) + startVector;
+			cubeInstance.name = "Source: " + "(" + source[0] + "," + source[1] + ")";
+		}
+		foreach (int[] sink in sinks) {
+			GameObject cubeInstance; 
+			cubeInstance = Instantiate(theCube);
+			cubeInstance.GetComponent<Renderer> ().material.color = Color.black;
+			cubeInstance.transform.position = Vector3.Scale((new Vector3(sink[0], 1, sink[1])), scaleVector) + startVector;
+			cubeInstance.name = "Sink: " + "(" + sink[0] + "," + sink[1] + ")";
+		}
+	
 		//Instantiate 1 cube for every trace
 		for (int i = 0; i < dictList.Count; i++) {
 			GameObject cubeInstance; 
 			cubeInstance = Instantiate(theCube);
+<<<<<<< HEAD
+			cubeInstance.GetComponent<Renderer> ().material.color = UnityEngine.Random.ColorHSV(0.5f, 0.6f, 0.2f, 1f, 1f, 1f);
+=======
 			//cubeInstance.GetComponent<Renderer> ().material.color = new Color((i*50)%255, 255, 150);
+>>>>>>> 6e60c904043896f910ce8ad8e87954c455275b08
             cubeInstance.transform.position = new Vector3(0, -10000, 0);
             cubeInstance.name = "Trace: " + i;
 			cubes.Add (cubeInstance);
@@ -75,10 +128,7 @@ public class AddCube : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextActionTime ) {
-
-            Debug.Log("Time step:" + step);
-
+		if (Time.time > nextActionTime && 0 == 0 ) {
             nextActionTime += stepDuration;
 
 			step++;
@@ -88,12 +138,20 @@ public class AddCube : MonoBehaviour {
             {
                 if(trace.ContainsKey(step))
                 {
+<<<<<<< HEAD
+                    cubes[cubeIndex].transform.position = Vector3.Scale(trace[step], scaleVector) + startVector;
+                }
+                else
+                {
+//                    cubes[cubeIndex].GetComponent<Renderer>().material.color = Color.blue;
+=======
                     //cubes[cubeIndex].GetComponent<Renderer>().material.color = Color.red;
                     cubes[cubeIndex].transform.localPosition = Vector3.Scale(trace[step], scaleVector) + startVector;
                 }
                 else
                 {
                     //cubes[cubeIndex].GetComponent<Renderer>().material.color = Color.blue;
+>>>>>>> 6e60c904043896f910ce8ad8e87954c455275b08
                 }
 
                 cubeIndex++;
@@ -101,8 +159,7 @@ public class AddCube : MonoBehaviour {
 
 		}
 	}
-
-
+		
 	public void parseCSV(string directoryPath) {
 		string[] files = Directory.GetFiles(directoryPath);
 		Dictionary<int, Vector3> itemDict;
@@ -122,7 +179,7 @@ public class AddCube : MonoBehaviour {
 				String[] line = sFileLines[i].Split(' ');
 				int time = int.Parse(line[2]);
 
-				itemDict.Add(time, new Vector3() { x = float.Parse(line[0]), y = 0, z = float.Parse(line[1]) });
+				itemDict.Add(time, new Vector3() { x = float.Parse(line[0]), y = 1, z = float.Parse(line[1]) });
 			}
 			dictList.Add(itemDict);
 		}	
---
+++ EvadeVR/Assets/Scripts/CollisionSameField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSameField : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		Debug.Log ("hallo");
+		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+}
